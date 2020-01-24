e4ea9a01fb56852167a4ca81e831fcada87981cb
Oliver <kicks.oliver@gmail.com>
2020-01-24 07:36:45 -0500

    Fixed Cors (finally)

--- PRBPServer/Global.asax.cs
+++ PRBPServer/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +16,21 @@ namespace PRBPServer
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.Headers.AllKeys.Contains("Origin") && Request.HttpMethod == "OPTIONS")
+            {
+                var origin = HttpContext.Current.Request.Headers["Origin"];
+
+                Response.Headers.Add("Access-Control-Allow-Origin", origin);
+                Response.Headers.Add("Access-Control-Allow-Headers", "content-type, withcredentials, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+                Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+                Response.Headers.Add("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
+
+                Response.Flush();
+            }
         }
     }
 }
--- PRBPServer/Startup.cs
+++ PRBPServer/Startup.cs
@@ -9,6 +9,7 @@ namespace PRBPServer
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }
